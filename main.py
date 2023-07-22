904d463818bc61db6bc9312a6bf1cc0db5876266
Sightoro <yuralon.skiy@mail.ru>
2023-07-22 17:43:05 +0300

    Снято ограничение на комплексность имени субъекта.

--- main.py
+++ main.py
@@ -3,12 +3,28 @@ import re
 import sqlite3
 
 
+def vocabulary_lib():
+    with open("input.txt", "r") as file:
+        regions_names = file.readline().split(',')
+    alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    region_dict_letter = {a: [] for a in alphabet}
+
+    for name in regions_names:
+        tmp_region = name.split()
+        for tmp_word in tmp_region:
+            if tmp_word[0] in region_dict_letter.keys() \
+                    and not (name in region_dict_letter[tmp_word[0]]):
+                region_dict_letter[tmp_word[0]].append(name.strip())
+    return region_dict_letter
+region_dict_letter = vocabulary_lib()
+
+
 # словарь с наменованиями субъектов и их полными названиями
 # для вывода в ответ
 def read_obl_txt():
     with open("obl.txt", "r") as file1:
-        dict_lvl = {0: [[a, a + " область"] for a in
-                        file1.read().split("\n")]}
+        dict_lvl = {a: a + " область" for a in
+                    file1.read().split("\n")}
     return dict_lvl
 
 
@@ -20,14 +36,8 @@ def read_republic_txt():
 
 def read_all_regions():
     with open("input.txt", "r") as file3:
-        regions = file3.readline().split(',')
-        full_reg_name = file3.readline().split(',')
-        fin = []
-        region_id = 1
-        for reg in regions:
-            fin.append([region_id, str(reg).strip()])
-            region_id += 1
-    return full_reg_name, regions
+        return file3.readline().split(',')
+
 
 
 # чтение входных данных
@@ -51,31 +61,34 @@ def close_db(cur):
 
 
 dict_level = read_obl_txt()
-dict_level[1] = [["Ненецкий", "Ненецкий автономный округ"],
-                 ["Ханты-Мансийский", "Ханты-Мансийский автономный \
-                        округ - Югра"],
-                 ["Чукотский", "Чукотский автономный округ"],
-                 ["Ямало-Ненецкий", "Ямало-Ненецкий автономный округ"],
-                 ["Еврейская", "Еврейская автономная область"]]
-dict_level[2] = [["Москва", "Москва"],
-                 ["Санкт-Петербург", "Санкт-Петербург"],
-                 ["Севастополь", "Севастополь"]]
-dict_level[3] = [["Алтайский", "Алтайский край"],
-                 ["Забайкальский", "Забайкальский край"],
-                 ["Камчатский", "Камчатский край"],
-                 ["Краснодарский", "Краснодарский край"],
-                 ["Красноярский", "Красноярский край"],
-                 ["Пермский", "Пермский край"],
-                 ["Приморский", "Приморский край"],
-                 ["Ставропольский", "Ставропольский край"],
-                 ["Хабаровский", "Хабаровский край"]]
-dict_level[4] = read_republic_txt()
+list4levels = [["Ненецкий", "Ненецкий автономный округ"],
+               ["Ханты-Мансийский-Югра", "Ханты-Мансийский"
+                                         " автономный округ - Югра"],
+               ["Чукотский", "Чукотский автономный округ"],
+               ["Ямало-Ненецкий", "Ямало-Ненецкий автономный округ"],
+               ["Еврейская", "Еврейская автономная область"],
+               ["Москва", "Москва"],
+               ["Санкт-Петербург", "Санкт-Петербург"],
+               ["Севастополь", "Севастополь"],
+               ["Алтайский", "Алтайский край"],
+               ["Забайкальский", "Забайкальский край"],
+               ["Камчатский", "Камчатский край"],
+               ["Краснодарский", "Краснодарский край"],
+               ["Красноярский", "Красноярский край"],
+               ["Пермский", "Пермский край"],
+               ["Приморский", "Приморский край"],
+               ["Ставропольский", "Ставропольский край"],
+               ["Хабаровский", "Хабаровский край"]]
+for i in read_republic_txt():
+    dict_level[i[0]] = i[1]
+for i in list4levels:
+    dict_level[i[0]] = i[1]
 
 # создание словаря, в котором ключ - первая буква,
 # а значение - название субъектаы
 alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 regions_dict = {a: [] for a in alphabet}
-full_reg_name, regions = read_all_regions()
+regions = read_all_regions()
 for region in regions:
     tmp_reg = region.split()
     for tmp_name in tmp_reg:
@@ -85,29 +98,75 @@ for region in regions:
             regions_dict[tmp_name[0]].append(region.strip())
 
 
-# функция для поиска по наименованию субъекта
-def search_by_level(iter_num, word, arr_of_levels,
-                    arr_of_addr, dict_addr):
+# поиск субъектов в словаре по первой букве названия
+def search_by_first_letter(word, letter_dict,
+                           arr_of_addr, dict_addr):
     ansi = []
-    if word in arr_of_levels[iter_num]:
-        for region_list in dict_addr[iter_num]:
-            try:
-                if arr_of_addr[arr_of_addr.index(word) + 1] == region_list[0]:
-                    ansi = region_list[1]
+    for region_in_dict in \
+            letter_dict[arr_of_addr[arr_of_addr.index(word) + 1][0]]:
+        try:
+            if arr_of_addr[arr_of_addr.index(word) + 1][:-2]\
+                    in region_in_dict:
+                ansi = dict_addr[region_in_dict]
+                return ansi
+
+        except IndexError:
+            if arr_of_addr[arr_of_addr.index(word) - 1] == "Народная" \
+                    or arr_of_addr[arr_of_addr.index(word) - 1] \
+                    == "народная":
+                if arr_of_addr[arr_of_addr.index(word) - 2][:-2]\
+                        in region_in_dict:
+                    ansi = dict_addr[region_in_dict]
                     return ansi
-            except IndexError:
-                if arr_of_addr[arr_of_addr.index(word) - 1] == region_list[0]:
-                    ansi = region_list[1]
-                    return ansi
-            try:
-                if not ansi:
-                    for region_list in dict_addr[iter_num]:
-                        if arr_of_addr[arr_of_addr.index(word) - 1] == \
-                                region_list[0]:
-                            ansi = region_list[1]
+            if arr_of_addr[arr_of_addr.index(word) - 1][:-2]\
+                    in region_in_dict:
+                ansi = dict_addr[region_in_dict]
+                return ansi
+        try:
+            if not ansi:
+                for region_in_dict in \
+                        letter_dict[arr_of_addr[arr_of_addr.index(word)
+                                                + 1][0]]:
+                    if arr_of_addr[arr_of_addr.index(word) - 1]\
+                            == "Народная" \
+                            or arr_of_addr[arr_of_addr.index(word) - 1]\
+                            == "народная":
+                        if arr_of_addr[arr_of_addr.index(word) - 2][:-2]\
+                                in region_in_dict:
+                            ansi = dict_addr[region_in_dict]
                             return ansi
-            except IndexError:
-                break
+                    if arr_of_addr[arr_of_addr.index(word) - 1][:-2]\
+                            in region_in_dict:
+                        ansi = dict_addr[region_in_dict]
+                        return ansi
+        except IndexError:
+            break
+
+
+# функция проверяет, является ли город федерального значения
+def is_it_federal_city(word, arr_of_addr):
+    cities = [["Москва", "Москва"],
+                ["Санкт-Петербург", "Санкт-Петербург"],
+                ["Севастополь", "Севастополь"]]
+    ansi = []
+    for city in cities:
+        try:
+            if arr_of_addr[arr_of_addr.index(word) + 1] == city[0]:
+                ansi = city[1]
+                return ansi
+        except IndexError:
+            if arr_of_addr[arr_of_addr.index(word) - 1] == city[0]:
+                ansi = city[1]
+                return ansi
+        try:
+            if not ansi:
+                for city in cities:
+                    if arr_of_addr[arr_of_addr.index(word) - 1] == \
+                            city[0]:
+                        ansi = city[1]
+                        return ansi
+        except IndexError:
+            break
 
 
 # функция обработки строки выдающая на выходе найденый регион
@@ -115,12 +174,13 @@ def kochegar(my_address):
     cur = open_db()
     # перечесление всех возможных ключевых слов
     address_levels = [
-        ["область", "обл", "Область", "о"],
-        ["Автономный", "авт", "автономный", "а", "округ", "окр", "ао"],
+        ["область", "обл", "Область", "о",
+         "Автономный", "авт", "автономный", "а", "округ", "окр", "ао",
+         "Край", "край", "кр",
+         "Республика", "респ", "республика", "рес", "регион", "Регион",
+         "рег", "р",
+         "округ", "окр"],
         ["город", "г", "Город", "Г", "значения"],
-        ["Край", "край", "кр"],
-        ["Республика", "респ", "республика", "рес"],
-        ["округ", "окр"],
         ["пос\.", "поселение", "р-н", "район", "с\/с", "сельсовет"],
         ["г", "город", "пгт", "рп", "кп\.", "гп\.", "п\.", "поселок",
          "аал", "арбан", "аул", "в-ки", "выселки",
@@ -144,13 +204,11 @@ def kochegar(my_address):
                     break
                 else:
                     continue
-            if not ans:
-                for i in range(5):
-                    if not ans:
-                        ans = search_by_level(i, elem, address_levels,
-                                              full_adr, dict_level)
-                    else:
-                        break
+            if elem in address_levels[1]:
+                ans = is_it_federal_city(elem, full_adr)
+            if not ans and elem in address_levels[0]:
+                ans = search_by_first_letter(elem, regions_dict,
+                                             full_adr, dict_level)
         answer.append(ans)
     close_db(cur)
     return answer
